0b6baeb59081e1950dfb2e60ec46e723bd8ca235
Luiz <luizfvidal13@hotmail.com>
2021-04-21 22:35:03 -0300

    Site - Login

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ use \Hcode\Model\User;
 use \Hcode\Model\Category;
 use \Hcode\Model\Product;
 use \Hcode\Model\Cart;
+use \Hcode\Model\Address;
 
 $app = new Slim();
 
@@ -119,6 +120,51 @@ $app->post("/cart/freight/", function(){
 	header("Location: /ecommerce/cart/");
 	exit;
 });
+
+//CHECKOUT DO SITE
+$app->get("/checkout/", function(){
+
+	User::verifyLogin(false);
+
+	$cart = Cart::getFromSession();
+	$address = new Address();
+	$page = new Page();
+	$page->setTpl("checkout", [
+		"cart"=>$cart->getValues(),
+		"address"=>$address->getValues()
+	]);
+});
+
+//TEMPLATE DE LOGIN DO SITE
+$app->get("/login/", function(){
+
+	$page = new Page();
+	$page->setTpl("login", [
+		"error"=>User::getError()
+	]);
+});
+
+//LOGIN DO SITE
+$app->post("/login/", function(){
+
+	try{		
+	User::login($_POST['login'], $_POST['password']);
+	}catch(Exception $e){
+		User::setError($e->getMessage());
+	}
+
+	header("Location: /ecommerce/checkout/");
+	exit;	
+});
+
+//LOGOUT DO SITE
+$app->get("/logout/", function(){
+
+	User::logout();
+	header("Location: /ecommerce/login/");
+	exit;
+});
+
 
 $app->get('/admin/', function() {
 	User::verifyLogin();
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -19,8 +19,8 @@ class User extends Model{
 
         $user = new User();
 
-        if(isset( $_SESSION[User::SESSION]) && (int) $_SESSION[User::SESSION]["iduser"] > 0 ){
-            $user->setData( $_SESSION[User::SESSION]);
+        if(isset( $_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION]["iduser"] > 0 ){
+            $user->setData($_SESSION[User::SESSION]);
         }
 
         return $user;
@@ -38,10 +38,15 @@ class User extends Model{
             return false;
         }else{
             if($inadmin === true && (bool)$_SESSION[User::SESSION]["inadmin"] === true){
+
                 return true;
+
             }else if($inadmin === false){
+
                 return true;
+
             }else{
+
                 return false;
             }
 
@@ -53,7 +58,7 @@ class User extends Model{
     public static function login($login, $password){
 
         $sql = new Sql();
-        $results = $sql->select("select * from tb_users where deslogin = :LOGIN", array( 
+        $results = $sql->select("select * from tb_users a INNER JOIN tb_persons b ON a.idperson = b.idperson WHERE a.deslogin = :LOGIN", array( 
             ":LOGIN"=>$login           
         ));
        if(count($results) === 0){
@@ -63,12 +68,14 @@ class User extends Model{
 
         if(password_verify($password, $data["despassword"]) === true){
             $user = new User();
+
+            $data['desperson'] = utf8_encode($data['desperson']);
+            
             $user->setData($data);          
 
             $_SESSION[User::SESSION] = $user->getValues();
 
             return $user;
-
 
         }else{
             throw new \Exception("Usuário inexistente ou senha inválida!");
@@ -77,17 +84,13 @@ class User extends Model{
 
     public static function verifyLogin($inadmin = true){
         
-        if (
-            !isset($_SESSION[User::SESSION])
-            || 
-            !$_SESSION[User::SESSION]
-            ||
-            !(int)$_SESSION[User::SESSION]["iduser"] > 0
-            ||
-            (bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
-            ) {
+        if(!User::checkLogin($inadmin)){
+            if($inadmin){
                 header("Location: /ecommerce/admin/login/");
-                exit;
+            }else{
+                header("Location: /ecommerce/login/");
+            }
+            exit;
         }
     }
 
@@ -106,9 +109,9 @@ class User extends Model{
 
         $results = $sql->select("call sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",
          array(
-        ":desperson"=>$this->getdesperson(),
+        ":desperson"=>utf8_decode($this->getdesperson()),
         ":deslogin"=>$this->getdeslogin(),
-        ":despassword"=>$this->getdespassword(),
+        ":despassword"=>User::getPasswordHash($this->getdespassword()),
         ":desemail"=>$this->getdesemail(),
         ":nrphone"=>$this->getnrphone(),
         ":inadmin"=>$this->getinadmin()
@@ -124,7 +127,10 @@ class User extends Model{
             ":iduser"=>$iduser
         ));
 
-        $this->setData($results[0]);
+        $data = $results[0];
+        $data['desperson'] = utf8_encode($data['desperson']);
+
+        $this->setData($data);
     }
 
     public function update(){
@@ -133,9 +139,9 @@ class User extends Model{
         $results = $sql->select("call sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",
          array(
         ":iduser"=>$this->getiduser(),
-        ":desperson"=>$this->getdesperson(),
+        ":desperson"=>utf8_decode($this->getdesperson()),
         ":deslogin"=>$this->getdeslogin(),
-        ":despassword"=>$this->getdespassword(),
+        ":despassword"=>User::getPasswordHash($this->getdespassword()),
         ":desemail"=>$this->getdesemail(),
         ":nrphone"=>$this->getnrphone(),
         ":inadmin"=>$this->getinadmin()
@@ -254,5 +260,41 @@ class User extends Model{
 		));
 
 	}
+
+    public static function setError($msg){
+
+        $_SESSION[User::ERROR] = $msg;
+    }
+
+    public static function getError(){
+        
+        $msg =  (isset($_SESSION[User::ERROR]) && $_SESSION[User::ERROR]) ? $_SESSION[User::ERROR] : "";           
+
+        User::clearError();
+        return $msg;
+    }
+
+    public static function clearError(){
+
+        $_SESSION[User::ERROR] = NULL;
+    }
+
+    public static function checkLoginExist($login){
+
+        $sql = new Sql();
+
+        $results = $sql->select("select * from tb_users where deslogin = :deslogin", [
+            ":deslogin"=>$login
+        ]);
+
+        return (count($results) > 0);
+    }
+
+    public static function getPasswordHash($password){
+
+        return password_hash($password, PASSWORD_DEFAULT, [
+            'cost'=>12
+        ]);
+    }
 }
 
