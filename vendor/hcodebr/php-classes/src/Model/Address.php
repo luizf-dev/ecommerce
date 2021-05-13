61f3627720b101ddccc06c13ac64a41cee0d2ec9
Luiz <luizfvidal13@hotmail.com>
2021-05-12 21:04:28 -0300

    Ajuste na tabela de endereços - Add um campo para o número

--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -45,11 +45,12 @@ class Address extends Model{
     public function save(){
 
         $sql = new Sql();
-        $results = $sql->select("call sp_addresses_save(:idaddress, :idperson, :desaddress, :descomplement,
+        $results = $sql->select("call sp_addresses_save(:idaddress, :idperson, :desaddress, :desnumber, :descomplement,
         :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
             ":idaddress"=>$this->getidaddress(),
             ":idperson"=>$this->getidperson(),
             ":desaddress"=>utf8_decode($this->getdesaddress()),
+            ":desnumber"=>$this->getdesnumber(),
             ":descomplement"=>utf8_decode($this->getdescomplement()),
             ":descity"=>utf8_decode($this->getdescity()),
             ":desstate"=>utf8_decode($this->getdesstate()),
